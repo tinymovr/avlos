6ddabd2464420673d8834dd6283dc15e44bdf1df
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-03-30 16:57:29 +0300

    initial rst output

--- avlos/generators/generator_rst.py
+++ avlos/generators/generator_rst.py
@@ -5,8 +5,10 @@ from rstcloth import RstCloth
 def process(instance, config):    
 
     d = RstCloth()
-
-
+    state = {"ep_counter": 1}
+    
+    traverse(instance, state, d)
+    d.write(config["output_file"])
     # d.title('Example Use')
     # d.newline()
     # d.h2('Contents')
@@ -16,3 +18,12 @@ def process(instance, config):
     # d.codeblock('#!/usr/bin/env')
 
     # d.print_content()
+
+
+def traverse(obj, state, d):
+    try:
+        for child in obj.children.values():
+            traverse(child, state, d)
+    except AttributeError:
+        d.newline()
+        d.h2(obj.name)
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -3,6 +3,7 @@ import yaml
 import importlib.resources
 from avlos.deserializer import deserialize
 import avlos.generators.generator_c as generator_c
+import avlos.generators.generator_rst as generator_rst
 import marshmallow
 import pint
 import unittest
@@ -23,3 +24,13 @@ class TestGeneration(unittest.TestCase):
                 }
             }
             generator_c.process(obj, config)
+
+    def test_rst_output_manual(self):
+        def_path_str = str(importlib.resources.files('tests').joinpath('definition/good_device.yaml'))
+        out_path_str = str(importlib.resources.files('tests').joinpath('outputs/impl.rst'))
+        with open(def_path_str) as system_description:
+            obj = deserialize(yaml.safe_load(system_description))
+            config = {
+                "output_file": out_path_str
+            }
+            generator_rst.process(obj, config)
