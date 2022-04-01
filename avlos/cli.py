0cb426fd2a9bd67a015ed68b38c1f0cc416e3e7f
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-04-01 16:58:55 +0300

    add cli

---
+++ avlos/cli.py
@@ -0,0 +1,43 @@
+"""Avlos CLI
+
+Usage:
+    avlos <spec_path> [--config=<config_path>]
+    avlos -h | --help
+    avlos --version
+
+Options:
+    --config=<config_path>  Path of the Avlos config file [default: ./avlos_config.yaml]
+"""
+
+import yaml
+from typing import Dict
+import logging
+import pkg_resources
+from docopt import docopt
+from avlos.deserializer import deserialize
+from avlos.processor import process_file
+
+shell_name = "Avlos"
+
+
+def run_cli():
+    version: str = pkg_resources.require("avlos")[0].version
+    arguments: Dict[str, str] = docopt(__doc__, version=shell_name + " " + str(version))
+
+    logger = configure_logging()
+
+    spec_path: str = arguments["<spec_path>"]
+    config_path: str = arguments["--config"]
+    with open(spec_path) as system_desc_stream:
+        obj = deserialize(yaml.safe_load(system_desc_stream))
+        process_file(obj, config_path)
+
+
+def configure_logging() -> logging.Logger:
+    """
+    Configures logging options and
+    generates a default logger instance.
+    """
+    logger = logging.getLogger("avlos")
+    logger.setLevel(logging.DEBUG)
+    return logger
--- setup.py
+++ setup.py
@@ -9,22 +9,20 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-setup(name='Avlos',
-    version='0.1',
-    description='Avlos Remote Object Templating System',
-    author='Yannis Chatzikonstantinou',
-    author_email='yannis@tinymovr.com',
-    url='https://www.tinymovr.com',
-    packages=find_packages(include=['avlos', 'avlos.*']),
-    python_requires='>=3.6',
-    install_requires=[
-        "marshmallow",
-        "pyyaml",
-        "pint"
-    ],
+setup(
+    name="Avlos",
+    version="0.1",
+    description="Avlos Remote Object Templating System",
+    author="Yannis Chatzikonstantinou",
+    author_email="yannis@tinymovr.com",
+    url="https://www.tinymovr.com",
+    packages=find_packages(include=["avlos", "avlos.*"]),
+    python_requires=">=3.6",
+    install_requires=["marshmallow", "pyyaml", "pint", "docopt"],
+    entry_points={"console_scripts": ["avlos=avlos.cli:run_cli"]},
     extras_require={
-        'generator_c': ["csnake"],
-        'generator_rst': ["rstcloth"],
+        "generator_c": ["csnake"],
+        "generator_rst": ["rstcloth"],
         #'generator_python': ["pint"]
     },
-     )
+)
