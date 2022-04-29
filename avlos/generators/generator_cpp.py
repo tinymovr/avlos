0965ad0aeb94fc4ebcfe15ae792da61d5646c72e
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-04-29 14:45:41 +0300

    fix cpp output

--- avlos/generators/generator_cpp.py
+++ avlos/generators/generator_cpp.py
@@ -1,8 +1,8 @@
-import os``
+import os
 from pathlib import Path
 from jinja2 import Environment, PackageLoader, select_autoescape
 import avlos
-from avlos.generators.type_map import c_type_map
+from avlos.generators.type_map import c_type_map as type_map
 
 
 env = Environment(loader=PackageLoader("avlos"), autoescape=select_autoescape())
@@ -14,28 +14,50 @@ def process(instance, config):
 
 
 def process_header(instance, config):
-    template = env.get_template("tinymovr.hpp")
-    with open(config["paths"]["output_header"], "w") as output_file:
-        print(template.render(instance=instance), file=output_file)
-    for object in instance["remote_attributes"]:
-        recurse_header(object, config)
+    template = env.get_template("tinymovr.hpp.jinja")
+    file = os.path.join(
+        os.path.dirname(config["paths"]["output_dir"]),
+        "tinymovr.hpp",
+    )
+    with open(file, "w") as output_file:
+        print(template.render(instance=instance, type_map=type_map), file=output_file)
+    for attr in instance.remote_attributes.values():
+        if hasattr(attr, "remote_attributes"):
+            recurse_header(attr, config)
 
 
 def recurse_header(remote_object, config):
-    template = env.get_template("remote_object.hpp")
-    file = os.path.join(Path(config["paths"]["output_header"]).resolve(), remote_object["name"])
+    template = env.get_template("remote_object.hpp.jinja")
+    file = os.path.join(
+        os.path.dirname(config["paths"]["output_dir"]),
+        remote_object.name + ".hpp",
+    )
     with open(file, "w") as output_file:
-        print(template.render(instance=remote_object), file=output_file)
+        print(
+            template.render(instance=remote_object, type_map=type_map), file=output_file
+        )
+    for attr in remote_object.remote_attributes.values():
+        if hasattr(attr, "remote_attributes"):
+            recurse_header(attr, config)
 
 
 def process_impl(instance, config):
-    template = env.get_template("tinymovr.cpp")
-    with open(config["paths"]["output_impl"], "w") as output_file:
-        print(template.render(instance=instance), file=output_file)
+    template = env.get_template("tinymovr.cpp.jinja")
+    file = os.path.join(
+        os.path.dirname(config["paths"]["output_dir"]),
+        "tinymovr.cpp",
+    )
+    with open(file, "w") as output_file:
+        print(template.render(instance=instance, type_map=type_map), file=output_file)
 
 
 def recurse_impl(remote_object, config):
-    template = env.get_template("remote_object.cpp")
-    file = os.path.join(Path(config["paths"]["output_impl"]).resolve(), remote_object["name"])
+    template = env.get_template("remote_object.cpp.jinja")
+    file = os.path.join(
+        os.path.dirname(config["paths"]["output_dir"]),
+        remote_object.name + ".cpp",
+    )
     with open(file, "w") as output_file:
-        print(template.render(instance=remote_object), file=output_file)
+        print(
+            template.render(instance=remote_object, type_map=type_map), file=output_file
+        )
