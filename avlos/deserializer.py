86b29fe6b5bb88d82b6f5966de62c6ebb13af720
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-05-05 22:36:23 +0300

    remove redundant

--- avlos/deserializer.py
+++ avlos/deserializer.py
@@ -1,12 +1,11 @@
 import json
 import hashlib
 from avlos.definition import RemoteNodeSchema
-from avlos.counter import make_counter, get_counter
+from avlos.counter import make_counter
 
 
 def deserialize(device_description):
     make_counter()
-    counter = get_counter()
     device_schema = RemoteNodeSchema()
     device_obj = device_schema.load(device_description)
     device_obj.hash_string = hash_string_from_string(json.dumps(device_description))
