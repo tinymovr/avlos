69eaedd1d8e6b82952ce7dd2ea5c0a9c45be3210
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-05-08 20:38:51 +0300

    use jinja for c generation

--- avlos/generators/generator_c.py
+++ avlos/generators/generator_c.py
@@ -1,195 +1,34 @@
-import os
-from os.path import relpath, dirname
-import avlos
-from avlos.generators.type_map import c_type_map
-from csnake import (
-    CodeWriter,
-    Enum,
-    Function,
-    FuncPtr,
-    Variable,
-    AddressOf,
-)
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+from avlos.datatypes import c_type_map as type_map
+from avlos.generators.filters import avlos_endpoints, as_include
+
+env = Environment(loader=PackageLoader("avlos"), autoescape=select_autoescape())
 
 
 def process(instance, config):
-    process_header(instance, config)
-    process_impl(instance, config)
+    env.filters['endpoints'] = avlos_endpoints
+    env.filters['as_include'] = as_include
 
-
-def process_header(instance, config):
-    cw_head = CodeWriter()
-    cw_head.add_autogen_comment()
-    cw_head.add_line("")
-    cw_head.add_line("#pragma once")
-    cw_head.add_line("")
-
-    # Imports
+    template = env.get_template("fw_endpoints.h.jinja")
     try:
-        for header in config["c_header_includes"]:
-            cw_head.include(header)
-        cw_head.add_line("")
+        includes = config["header_includes"]
     except KeyError:
-        pass
-
-    # Enums
-    enum_ret = Enum("Avlos_Return", prefix="AVLOS_RET_", typedef=True)
-    enum_ret.add_value("NOACTION", 0)
-    enum_ret.add_value("READ", 1)
-    enum_ret.add_value("WRITE", 2)
-    cw_head.add_enum(enum_ret)
-    cw_head.add_line("")
-
-    enum_dir = Enum("Avlos_Command", prefix="AVLOS_CMD_", typedef=True)
-    enum_dir.add_value("WRITE", 0)
-    enum_dir.add_value("READ", 1)
-    cw_head.add_enum(enum_dir)
-    cw_head.add_line("")
-
-    # Hash value
-    v = Variable("avlos_proto_hash", "static uint32_t", value=instance.hash_int32)
-    cw_head.add_variable_initialization(v)
-    cw_head.add_line("")
-
-    # Function prototypes
-    state = {"prefix": ""}
-    traverse_header(instance, state, cw_head)
-
-    # Function list
-    state = {
-        "f_list": [],
-        "prefix": "",
-    }
-    traverse_function_list(instance, state, cw_head)
-    output_function_array(state["f_list"], cw_head)
-
-    # Write out
+        includes = []
     with open(config["paths"]["output_header"], "w") as output_file:
-        print(cw_head, file=output_file)
+        print(
+            template.render(instance=instance, type_map=type_map, includes=includes),
+            file=output_file,
+        )
 
-
-def process_impl(instance, config):
-    cw_impl = CodeWriter()
-    cw_impl.add_autogen_comment()
-    cw_impl.add_line("")
-
-    # Includes
+    template = env.get_template("fw_endpoints.c.jinja")
     try:
-        for header in config["c_impl_includes"]:
-            cw_impl.include(header)
+        includes = config["impl_includes"]
     except KeyError:
-        pass
-    cw_impl.include(
-        relpath(
-            config["paths"]["output_header"], dirname(config["paths"]["output_impl"])
-        )
-    )
-    cw_impl.add_line("")
-
-    # # Implementations
-    state = {"prefix": ""}
-    traverse_impl(instance, state, cw_impl)
-
-    # Write out
+        includes = []
     with open(config["paths"]["output_impl"], "w") as output_file:
-        print(cw_impl, file=output_file)
-
-
-def traverse_function_list(obj, state, cw):
-    try:
-        current_prefix = state["prefix"]
-        for child in obj.remote_attributes.values():
-            state["prefix"] = "{}{}_".format(current_prefix, obj.name)
-            traverse_function_list(child, state, cw)
-        state["prefix"] = current_prefix
-    except AttributeError:
-        f_name = get_f_name("{}{}".format(state["prefix"], obj.name))
-        state["f_list"].append(
-            AddressOf(Variable(f_name, FuncPtr("uint8_t", arguments=get_args())))
+        print(
+            template.render(instance=instance, type_map=type_map, includes=includes),
+            file=output_file,
         )
 
-
-def output_function_array(f_list, cw):
-    v = Variable(
-        "avlos_endpoints", FuncPtr("static uint8_t", arguments=get_args()), value=f_list
-    )
-    cw.add_variable_initialization(v)
-
-
-def traverse_header(obj, state, cw):
-    try:
-        current_prefix = state["prefix"]
-        for child in obj.remote_attributes.values():
-            state["prefix"] = "{}{}_".format(current_prefix, obj.name)
-            traverse_header(child, state, cw)
-        state["prefix"] = current_prefix
-    except AttributeError:
-        f_name = get_f_name("{}{}".format(state["prefix"], obj.name))
-        cw.start_comment()
-        cw.add_line(f_name)
-        cw.add_line("")
-        cw.add_line(obj.description)
-        cw.add_line("")
-        cw.add_line("@param buffer")
-        cw.add_line("@param buffer_len")
-        cw.end_comment()
-        fun = Function(f_name, "uint8_t", arguments=get_args())
-        cw.add_function_prototype(fun)
-        cw.add_line("")
-
-
-def traverse_impl(obj, state, cw):
-    try:
-        current_prefix = state["prefix"]
-        for child in obj.remote_attributes.values():
-            state["prefix"] = "{}{}_".format(current_prefix, obj.name)
-            traverse_impl(child, state, cw)
-        state["prefix"] = current_prefix
-    except AttributeError:
-        f_name = get_f_name("{}{}".format(state["prefix"], obj.name))
-        fun = Function(f_name, "uint8_t", arguments=get_args())
-        fun.codewriter = CodeWriter()
-        v = Variable("v", c_type_map[obj.dtype])
-        fun.codewriter.add_variable_declaration(v)
-
-        try:
-            c_getter = obj.c_getter
-        except AttributeError:
-            c_getter = None
-
-        try:
-            c_setter = obj.c_setter
-        except AttributeError:
-            c_setter = None
-
-        # TODO: Make implementation using safe primitives
-        token = "if"
-        if c_getter:
-            fun.codewriter.add_line("if (AVLOS_CMD_READ == cmd) {")
-            fun.codewriter.add_line("    v = {}();".format(obj.c_getter))
-            fun.codewriter.add_line("    *buffer_len = sizeof(v);")
-            fun.codewriter.add_line("    memcpy(buffer, &v, sizeof(v));")
-            fun.codewriter.add_line("    return AVLOS_RET_READ;")
-            fun.codewriter.add_line("}")
-            token = "else if"
-        if c_setter:
-            fun.codewriter.add_line(token + " (AVLOS_CMD_WRITE == cmd) {")
-            fun.codewriter.add_line("    memcpy(&v, buffer, sizeof(v));")
-            fun.codewriter.add_line("    {}(v);".format(c_setter))
-            fun.codewriter.add_line("    return AVLOS_RET_WRITE;")
-            fun.codewriter.add_line("}")
-        fun.codewriter.add_line("return AVLOS_RET_NOACTION;")
-
-        cw.add_function_definition(fun)
-        cw.add_line("")
-
-
-def get_f_name(accessor):
-    return "avlos_{}".format(accessor)
-
-
-def get_args():
-    arg1 = Variable("buffer", "uint8_t *")
-    arg2 = Variable("buffer_len", "uint8_t *")
-    arg3 = Variable("cmd", "Avlos_Command")
-    return arg1, arg2, arg3
