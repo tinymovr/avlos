f561b2977cd9547184fdc3c1658766c37dde2b3b
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-05-09 15:04:39 +0300

    add dbc output

---
+++ avlos/generators/generator_dbc.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+from avlos.generators.filters import avlos_endpoints
+
+env = Environment(loader=PackageLoader("avlos"), autoescape=select_autoescape())
+
+
+def process(instance, config):
+    env.filters["endpoints"] = avlos_endpoints
+
+    template = env.get_template("device.dbc.jinja")
+    with open(config["paths"]["output_file"], "w") as output_file:
+        print(
+            template.render(instance=instance),
+            file=output_file,
+        )
