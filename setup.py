57080f00030142eec6191f86c112169cf66de944
Ioannis Chatzikonstantinou <ihatz@iti.gr>
2022-05-26 16:41:04 +0300

    simplify requirements

--- setup.py
+++ setup.py
@@ -9,11 +9,6 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-extras_generator_c = ["csnake"]
-extras_generator_rst = ["rstcloth"]
-extras_generator_cpp = ["jinja2"]
-extras_all = extras_generator_c + extras_generator_rst + extras_generator_cpp
-
 setup(
     name="Avlos",
     version="0.1",
@@ -23,12 +18,6 @@ setup(
     url="https://www.tinymovr.com",
     packages=find_packages(include=["avlos", "avlos.*"]),
     python_requires=">=3.6",
-    install_requires=["marshmallow", "pyyaml", "pint", "docopt"],
-    entry_points={"console_scripts": ["avlos=avlos.cli:run_cli"]},
-    extras_require={
-        "generator_c": extras_generator_c,
-        "generator_rst": extras_generator_rst,
-        "generator_cpp": extras_generator_cpp,
-        'all': extras_all
-    },
+    install_requires=["marshmallow", "pyyaml", "pint", "docopt", "jinja2"],
+    entry_points={"console_scripts": ["avlos=avlos.cli:run_cli"]}
 )
