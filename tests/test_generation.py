f7804ae053ec659472a2dd82577231575997d328
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-06-11 23:14:42 +0300

    add checking of generated files with cppcheck

--- tests/test_generation.py
+++ tests/test_generation.py
@@ -1,11 +1,11 @@
 import yaml
+import subprocess
 import importlib.resources
 from avlos.deserializer import deserialize
 from avlos.processor import process_file
 import avlos.generators.generator_c as generator_c
+import avlos.generators.generator_cpp as generator_cpp
 import avlos.generators.generator_rst as generator_rst
-import marshmallow
-import pint
 import unittest
 from pprint import pprint
 
@@ -32,6 +32,40 @@ class TestGeneration(unittest.TestCase):
                 "c_includes": {"src/common.h"},
             }
             generator_c.process(obj, config)
+
+            result = subprocess.run(
+                ["cppcheck", "--error-exitcode=1", header_path_str, impl_path_str],
+                stdout=subprocess.DEVNULL,
+            )
+            self.assertEqual(result.returncode, 0)
+
+    def test_cpp_output_manual(self):
+        def_path_str = str(
+            importlib.resources.files("tests").joinpath("definition/good_device.yaml")
+        )
+        header_path_str = str(
+            importlib.resources.files("tests").joinpath("outputs/device.hpp")
+        )
+        impl_path_str = str(
+            importlib.resources.files("tests").joinpath("outputs/device.cpp")
+        )
+        with open(def_path_str) as device_desc_stream:
+            obj = deserialize(yaml.safe_load(device_desc_stream))
+            config = {
+                "hash_string": "0x9e8dc7ac",
+                "paths": {
+                    "output_header": header_path_str,
+                    "output_impl": impl_path_str,
+                },
+                "header_includes": {"codecs.hpp"},
+            }
+            generator_cpp.process(obj, config)
+
+            result = subprocess.run(
+                ["cppcheck", "--error-exitcode=1", header_path_str, impl_path_str],
+                stdout=subprocess.DEVNULL,
+            )
+            self.assertEqual(result.returncode, 0)
 
     def test_rst_output_manual(self):
         def_path_str = str(
