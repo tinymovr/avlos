cdd0cb1e391ced7c383ac33b743eb339298d1ad8
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-06-11 23:27:46 +0300

    update url

--- tests/test_deserialization.py
+++ tests/test_deserialization.py
@@ -20,7 +20,7 @@ class TestDeserialization(unittest.TestCase):
 
     def test_success_url(self):
         device_desc_string = urllib.request.urlopen(
-            "https://raw.githubusercontent.com/tinymovr/avlos/main/tests/definition/good_device.yaml?token=GHSAT0AAAAAABRW7AJHDUWSS3KRBI4A27FAYUCJM7Q"
+            "https://raw.githubusercontent.com/tinymovr/avlos/main/tests/definition/good_device.yaml?token=GHSAT0AAAAAABOYBI3BFKF6LTL74MM5MVWGYVE7KVQ"
         ).read()
         obj = deserialize(yaml.safe_load(device_desc_string))
         obj._channel = DummyChannel()
