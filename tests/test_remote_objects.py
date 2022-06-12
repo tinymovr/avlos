0742f16bd7deb40b1273842ae1c3965dc5a23741
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-06-12 13:27:03 +0300

    test a bunch more values and attributes

--- tests/test_remote_objects.py
+++ tests/test_remote_objects.py
@@ -1,11 +1,11 @@
 import yaml
 import importlib.resources
 from avlos.deserializer import deserialize
-import marshmallow
-import pint
+from avlos.unit_field import get_registry
 import unittest
 from tests.dummy_channel import DummyChannel
 
+_reg = get_registry()
 
 class TestRemoteObjects(unittest.TestCase):
     def test_properties(self):
@@ -17,4 +17,12 @@ class TestRemoteObjects(unittest.TestCase):
             obj._channel = DummyChannel()
 
             self.assertEqual(0, obj.Vbus)
+            obj._channel.set_value(12.0)
+            self.assertEqual(12.0 * _reg("volt"), obj.Vbus)
+            obj._channel.set_value(24.0)
+            self.assertEqual(24.0 * _reg("volt"), obj.Vbus)
+
+            obj._channel.set_value(0.1)
+            self.assertEqual(0.1 * _reg("ohm"), obj.motor.R)
+
             obj.controller.set_pos_vel_setpoints(0, 0)
