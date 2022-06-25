b6fccbb742f6d6318dadd8139f32a21eb095c029
Yannis Chatzikonstantinou <yconst@gmail.com>
2022-06-25 22:32:17 +0300

    allow option to discard root node name by default

--- avlos/mixins/named_node.py
+++ avlos/mixins/named_node.py
@@ -2,9 +2,10 @@ from functools import cached_property
 
 class NamedNode:
 
-    def __init__(self, name):
+    def __init__(self, name, include_base_name=False):
         self._parent = None
         self.name = name
+        self.include_base_name = include_base_name
 
     # this kinda stinks, cause it is implemented elsewhere
     # @cached_property
@@ -14,6 +15,8 @@ class NamedNode:
     @cached_property
     def full_name(self):
         try:
-            return "{}.{}".format(self._parent.full_name, self.name)
+            return "{}.{}".format(self._parent.full_name, self.name).strip(".")
         except AttributeError:
-            return self.name
+            if self.include_base_name:
+                return self.name
+            return ""
