7af4ec08071ff29ad9db1b7045575b3ce2855a92
Ioannis Chatzikonstantinou <yconst@gmail.com>
2022-09-27 15:01:53 +0300

    improve function docstrings

--- avlos/channel.py
+++ avlos/channel.py
@@ -12,6 +12,11 @@ class BaseChannel:
         Send data to endpoint ep_id. Implement this to
         send data contained in the data byte array, to
         the endpoint with id ep_id
+        Arguments:
+            data: a bytearray containing the data to
+                  be sent
+            ep_id: an integer representing the endpoint
+                   ID to send to
         """
         raise NotImplementedError
 
@@ -21,19 +26,42 @@ class BaseChannel:
         to receive data from endpoint with id ep_id. The
         implementation should timeout after a period equal
         to timeout has elapsed.
+        Arguments:
+            ep_id: an integer representing the endpoint ID
+                   to listen to for data
+            timeout: an integer indicating a timeout for
+                     receiving any data
+        Returns:
+            A bytearray containing the received data
         """
         raise NotImplementedError
 
     @cached_property
     def max_ep_id(self):
         """
-        Max value that the endpoint can get for this channel.
+        Get the max value that the endpoint can get for
+        this channel.
+        Returns:
+            An integer representing the max endpoint ID
+            value
+        """
+        raise NotImplementedError
+
+    @cached_property
+    def max_packet_size(self):
+        """
+        Get the max size in bytes of the packet that this
+        channel can transmit/receive.
+        Returns:
+            An integer representing the max packet size
         """
         raise NotImplementedError
 
     @cached_property
     def serializer(self):
         """
-        Return a data codec appropriate for the channel.
+        Return a data codec appropriate for this channel.
+        Returns:
+            The data codec instance
         """
         raise NotImplementedError
