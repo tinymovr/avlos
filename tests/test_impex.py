758fc1466df131062b31dffd3802df9020ca3f45
Yannis Chatzikonstantinou <info@tinymovr.com>
2022-12-25 19:44:59 +0200

    update tests

---
+++ tests/test_impex.py
@@ -0,0 +1,26 @@
+import yaml
+import importlib.resources
+from avlos.deserializer import deserialize
+from avlos.unit_field import get_registry
+import unittest
+from tests.dummy_channel import DummyChannel
+
+_reg = get_registry()
+
+
+class TestImpex(unittest.TestCase):
+    
+    def test_read_remote_properties(self):
+        def_path_str = str(
+            importlib.resources.files("tests").joinpath("definition/good_device.yaml")
+        )
+        with open(def_path_str) as device_description:
+
+            obj = deserialize(yaml.safe_load(device_description))
+            obj._channel = DummyChannel()
+
+            obj._channel.set_value(0)
+
+            values = obj.export_values()
+
+            obj.import_values(values)
