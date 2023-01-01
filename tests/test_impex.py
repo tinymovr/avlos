7349a11acbe64cfb39761e4fb3eb1c905813276f
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-01-01 20:55:31 +0200

    add test

--- tests/test_impex.py
+++ tests/test_impex.py
@@ -10,7 +10,7 @@ _reg = get_registry()
 
 class TestImpex(unittest.TestCase):
     
-    def test_read_remote_properties(self):
+    def test_import_export(self):
         def_path_str = str(
             importlib.resources.files("tests").joinpath("definition/good_device.yaml")
         )
@@ -24,3 +24,27 @@ class TestImpex(unittest.TestCase):
             values = obj.export_values()
 
             obj.import_values(values)
+
+    def test_import_export_root_object(self):
+        def_path_str = str(
+            importlib.resources.files("tests").joinpath("definition/good_device.yaml")
+        )
+        with open(def_path_str) as device_description:
+            
+            obj = deserialize(yaml.safe_load(device_description))
+            obj._channel = DummyChannel()
+
+            obj._channel.set_value(0)
+
+            func = obj.move_to
+
+            root = func.root
+
+            self.assertEqual(obj, root)
+
+            values = root.export_values()
+
+            root.import_values(values)
+
+            
+ 
