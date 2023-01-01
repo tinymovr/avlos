92123f29015b6549cd9e2c939dc6cb11af411e48
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-01-02 01:18:54 +0200

    streamline test

--- tests/test_impex.py
+++ tests/test_impex.py
@@ -1,50 +1,31 @@
+import json
 import yaml
 import importlib.resources
 from avlos.deserializer import deserialize
 from avlos.unit_field import get_registry
+from avlos.json_codec import AvlosEncoder
 import unittest
 from tests.dummy_channel import DummyChannel
-
-_reg = get_registry()
 
 
 class TestImpex(unittest.TestCase):
     
-    def test_import_export(self):
-        def_path_str = str(
-            importlib.resources.files("tests").joinpath("definition/good_device.yaml")
-        )
-        with open(def_path_str) as device_description:
-
-            obj = deserialize(yaml.safe_load(device_description))
-            obj._channel = DummyChannel()
-
-            obj._channel.set_value(0)
-
-            values = obj.export_values()
-
-            obj.import_values(values)
-
     def test_import_export_root_object(self):
         def_path_str = str(
             importlib.resources.files("tests").joinpath("definition/good_device.yaml")
         )
         with open(def_path_str) as device_description:
-            
             obj = deserialize(yaml.safe_load(device_description))
             obj._channel = DummyChannel()
-
             obj._channel.set_value(0)
-
-            func = obj.move_to
-
+            func = obj.motor.remote_attributes["R"]
             root = func.root
-
             self.assertEqual(obj, root)
-
             values = root.export_values()
+            json_string = json.dumps(values, cls=AvlosEncoder)
+            imported_values = json.loads(json_string)
+            root.import_values(imported_values)
 
-            root.import_values(values)
 
             
  
