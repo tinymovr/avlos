dc85b8e03546079c2942f0985551bc1a71b964f1
Yannis Chatzikonstantinou <info@tinymovr.com>
2023-01-02 01:17:44 +0200

    use set_value_with_string instead

--- avlos/mixins/impex_node.py
+++ avlos/mixins/impex_node.py
@@ -3,7 +3,7 @@ class ImpexNode:
         # TODO: Assert correct type
         try:
             if "export" in self.meta and self.meta["export"] == True:
-                self.set_value(data)
+                self.set_value_with_string(data)
         except AttributeError:
             for name, attr in self.remote_attributes.items():
                 try:
