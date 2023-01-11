d81151f7be038eab959927b26d0eb3e20fffebb0
Ioannis Chatzikonstantinou <yconst@gmail.com>
2023-01-11 17:03:05 +0200

    add tests

---
+++ tests/test_counter.py
@@ -0,0 +1,30 @@
+from avlos.counter import get_counter, make_counter
+
+import unittest
+
+
+class TestCounter(unittest.TestCase):
+
+    def test_make_counter_return(self):
+        self.assertIsNone(make_counter())
+
+    def test_get_counter(self):
+        self.assertIsNone(get_counter())
+        make_counter()
+        self.assertIsNotNone(get_counter())
+
+    def test_make_counter(self):
+        make_counter()
+        counter1 = get_counter()
+        counter2 = get_counter()
+        make_counter()
+        counter3 = make_counter()
+        self.assertEqual(counter1, counter2)
+        self.assertNotEqual(counter1, counter3)
+
+    def test_counter_next(self):
+        make_counter()
+        counter = get_counter()
+        self.assertEqual(0, counter.count)
+        self.assertEqual(1, counter.next())
+        self.assertEqual(counter.next(), counter.count+1)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,8 +1,12 @@
-from avlos.generators.filters import as_include
+from avlos.generators.filters import as_include, file_from_path
 import unittest
 
 
 class TestFunctions(unittest.TestCase):
+
+    def test_file_from_path_filter(self):
+         self.assertEqual("test_functions.py", file_from_path(__file__))
+
     def test_jinja_as_include_filter(self):
         self.assertEqual("<test.h>", as_include("test.h"))
         self.assertEqual("<test.h>", as_include("<test.h>"))
